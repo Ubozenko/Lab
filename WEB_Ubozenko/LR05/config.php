c0aa8507a4b554b10cd304c2b144573bddf2db47
Ubozenko <over.yy16@gmail.com>
2023-12-20 16:14:06 +0200

    add lr web

---
+++ WEB_Ubozenko/LR05/config.php
@@ -0,0 +1,14 @@
+<?php
+$users = array(
+    array(
+        'login' => 'Ubozenko',
+        'password' => 'password1',
+        'name' => 'Користувач 1'
+    ),
+    array(
+        'login' => 'user1',
+        'password' => 'password1',
+        'name' => 'Користувач 2'
+    ),
+);
+?>
